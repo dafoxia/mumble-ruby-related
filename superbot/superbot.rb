fe45b6fc11242faa054aaa6eddf8b866d6c48ca8
Natenom <natenom@natenom.name>
2014-08-02 19:34:02 +0200

    Added stats command

--- superbot/superbot.rb
+++ superbot/superbot.rb
@@ -308,7 +308,8 @@ class MumbleMPD
 								+ "#{cc}<b>ch</b> Let the bot switch into your channel.<br />" \
 								+ "#{cc}<b>song</b> Show the currently played song information.<br />If this information is empty, try #{cc}file instead.<br />" \
 								+ "#{cc}<b>file</b> Show the filename of the currently played song if #{cc}song does not contain useful information.<br />" \
-						        + "#{cc}<b>help</b> Shows this help.<br />" \
+								+ "#{cc}<b>help</b> Shows this help.<br />" \
+								+ "#{cc}<b>stats</b> Shows some MPD statistics." \
 								+ "<hr /><span style='color:grey;font-size:10px;'><a href='http://wiki.natenom.com/w/Superbot'>See here for my documentation.</a></span>")
 					end
 					if message.match(/^seek [+-]?[0-9]{1,3}$/)
@@ -548,6 +549,10 @@ class MumbleMPD
 					if message == 'playlist'
 						songlist = @mpd.songs
 						puts songlist.inspect
+					end
+					if message == 'stats'
+						stats = @mpd.stats
+						@cli.text_user(msg.actor, "MPD stats:<br />#{stats.inspect}")
 					end
 					if message == 'playlists'
 						text_out = ""
